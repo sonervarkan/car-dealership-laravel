a0ac8dbaa8f15d194d0ecde6d7d035e074f84f93
sonervarkan <sonervarkan1455@yahoo.com>
2025-10-29 18:39:56 +0300

    Refactor slide navigation and automatic transition
    Updated slide transition logic and comments.

--- public/script/filter.js
+++ public/script/filter.js
@@ -6,24 +6,24 @@ const $prev = $('.prev');
 let currentIndex = 0;
 const totalSlides = $slide.length;
 
-// Başlangıçta ilk slide aktif
+
 $slide.hide().eq(0).show();
 
-// İleri butonu
+
 $next.on('click', function() {
 $slide.eq(currentIndex).fadeOut(100);
 currentIndex = (currentIndex + 1) % totalSlides;
 $slide.eq(currentIndex).fadeIn(400);
 });
 
-// Geri butonu
+
 $prev.on('click', function() {
 $slide.eq(currentIndex).fadeOut(100);
 currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
 $slide.eq(currentIndex).fadeIn(400);
 });
 
-// Otomatik geçiş
+
 setInterval(function() {
 $next.click();
-}, 3000); // her 3 saniyede bir
+}, 3000); 
